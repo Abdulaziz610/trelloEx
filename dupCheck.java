183d7d7ce130625176ac4857a3a810defb076500
Abdulaziz610 <abdulazizalnasseri1998@gmail.com>
2023-01-05 04:55:23 -0800

    newEx
    Duplicate String

---
+++ dupCheck.java
@@ -0,0 +1,28 @@
+package questionO;
+
+import java.util.HashSet;
+
+import java.util.Set;
+
+public class dupCheck {
+
+	public static void main(String[] args) {
+
+		String[] names = { "Atyab", "Fatima", "Saeed", "Malak", "Fatima", "Daniyal", "Abdullah", "Abdullah" };
+		for (int i = 0; i < names.length; i++) {
+			int count = 1;
+			for (int j = i + 1; j < names.length; j++) {
+				if (names[i].equals(names[j])) {
+					count++;
+					names[j] = "";
+				}
+			}
+			if (!names[i].equals("")) {
+				System.out.println(names[i] + " appears " + count + " times.");
+			}
+
+		}
+
+	}
+
+}
